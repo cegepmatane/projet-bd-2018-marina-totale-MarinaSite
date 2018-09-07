44862695dda542e6dfd6243ce3ff1322248ae75d
Florent F <florent.fli@gmail.com>
2018-09-07 14:43:52 -0400

    architechture début site public

---
+++ Site public/header.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Florent
+ * Date: 07/09/2018
+ * Time: 14:33
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>MarinaConnect</title>
+</head>
+
+<body>
+
