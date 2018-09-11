aa7f58cb054055c380439f5a649c550645dafa92
Florent F <florent.fli@gmail.com>
2018-09-11 15:05:30 -0400

    #3 Vue Formulaire reservation client

--- Site public/reserverEmplacement.php
+++ Site public/reserverEmplacement.php
@@ -9,20 +9,46 @@ include "header.php";
 ?>
 
 
-<fieldset>
-    <legend> Ajouter un Auteur</legend>
-    <label>Nom auteur :
-        <input name="nomAuteur" type="text" size="18" value="">
+<fieldset class="formulaireClient">
+    <legend>Réserver un emplacement</legend>
+    <label>Nom :
+        <input name="nomClient" type="text" size="18" value="">
     </label>
-    <?php if (isset($erreurs['nomAuteur'])) echo '<div class="alert alert-danger">'.$erreurs['nomAuteur'].'</div>'; ?>
-
-    <label>Prenom auteur :
-        <input name="prenomAuteur" type="text" size="18" value="">
+    </br>
+    <label>Prenom :
+        <input name="prenomClient" type="text" size="18" value="">
     </label>
-    <?php if (isset($erreurs['prenomAuteur'])) echo '<div class="alert alert-danger">'.$erreurs['prenomAuteur'].'</div>'; ?>
+    </br>
+    <label>Longueur bateau (en m):
+        <input name="longueurBateau" type="number" size="18" value="">
+    </label>
+    </br>
+    <label>Largeur bateau (en m):
+        <input name="largeurBateau" type="number" size="5" value="">
+    </label>
+    </br>
+    <label>Date d'arrivé :
+        <input name="prenomClient" type="date" size="5" value="">
+    </label>
+    </br>
+    <label>Date de départ :
+        <input name="dateDepart" type="date" size="18" value="">
+    </label>
+    </br>
+    <label>Electricité :
+        <input name="electricité" type="checkbox" size="18" value="">
+    </label>
+    </br>
+    <label>Éssence :
+        <input name="essence" type="checkbox" size="18" value="">
+    </label>
+    </br>
+    <label>Vidange :
+        <input name="vidange" type="checkbox" size="18" value="">
+    </label>
+    </br></br>
 
-
-    <input type="submit" name="AddAuteur" value ="Add">
+    <input type="submit" name="send" value ="ENREGISTRER">
 </fieldset>
 
 <?php include "footer.php" ?>
