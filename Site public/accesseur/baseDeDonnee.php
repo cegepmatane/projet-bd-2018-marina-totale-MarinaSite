5a2d72e23204dd0458bcd77f43f3f9df73f4a318
Florent F <florent.fli@gmail.com>
2018-09-18 16:50:41 -0400

    #5 changement utilisateur BDD

--- Site public/accesseur/baseDeDonnee.php
+++ Site public/accesseur/baseDeDonnee.php
@@ -2,7 +2,7 @@
 $host = "host = 158.69.113.110";
 $port = "port = 5432";
 $dbname = "dbname = MarinaBateauDB";
-$credentials = "user=webMestre password=bdd2018Marina";
+$credentials = "user=webmestre password=bdd2018Marina";
 
 $db = pg_connect("$host $port $dbname $credentials");
 
--- Site public/modele/Service.php
+++ Site public/modele/Service.php
@@ -123,5 +123,5 @@ class Service{
         $this->idemplacement = $idemplacement;
     }
 
-    
+
 }
