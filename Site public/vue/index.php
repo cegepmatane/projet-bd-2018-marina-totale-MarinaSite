9804627d7d52ef56c3a98d6e7a045d6b79162f9d
Hugo BLANCHARD <zaturgo@gmail.com>
2018-09-21 13:59:39 -0400

    Ajout de la base de la connection et redirections

--- Site public/vue/index.php
+++ Site public/vue/index.php
@@ -1,7 +1,57 @@
-<?php include 'header.php'; ?>
+<?php include 'header.php';
+$PSEUDO = null;
+$PASS = 0;
+$MDP = null;
+if ((isset($_POST['mot_de_passe']))){
+    $MDP = $_POST['mot_de_passe'];
+}
+if ((isset($_POST['pseudo']))){
+    $PSEUDO = $_POST['pseudo'];
+}
+?>
 
-    <h1>Marina Connect</h1>
-    <a href="reserverEmplacement.php">Réserver un emplacement</a>
+<div class="formulaireClient">
+    <fieldset>
+        <legend>Connexion à MarinaConnect</legend>
 
+        <form action="index.php" method="post">
+            <p> <label>Identifiant: </label>
+                <input type="text" name="pseudo" />
+                <label>Mot de passe: </label>
+                <input type="password" name="mot_de_passe" />
+                <input type="submit" name="send" value="CONNECTION">
+            </p>
+        </form>
+        <a href="ajouterUtilisateur.php">Creer un compte</a>
+</div>
 
-<?php include "footer.php"; ?>
+<?php
+
+if (($PSEUDO == "user") &&($MDP ==  "user"))
+{
+    $PASS = 1;
+    ?>
+    Redirection en cours user
+    <?php
+    header('Location: reserverEmplacement.php');
+    exit();
+}
+if (($PSEUDO == 'admin')&&($MDP == 'admin'))
+{
+    $PASS = 1;
+    ?>
+    Redirection en cours admin
+    <?php
+    header('Location: partieGerant.php');
+    exit();
+}
+else if(($PSEUDO != null)&&($MDP != null)&&($PASS ==0))
+{
+    ?>
+    Mot de passe incorrect
+    <?php
+}
+?>
+
+<?php include 'footer.php'; ?>
+
