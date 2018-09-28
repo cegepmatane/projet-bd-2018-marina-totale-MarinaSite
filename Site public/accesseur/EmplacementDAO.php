8e5d4640eac56cccdc2ca0bd1bbbc2909ef13ffd
Antoine <aherkens@gmail.com>
2018-09-28 14:02:33 -0400

    #5 ajouter, modifier supprimer emplacementDAO BDD

--- Site public/accesseur/EmplacementDAO.php
+++ Site public/accesseur/EmplacementDAO.php
@@ -12,4 +12,56 @@ Class EmplacementDAO{
         return $requeteListerEmplacement->fetchAll(PDO::FETCH_OBJ);
     }
 
+    public function ajouterEmplacement(Emplacement $emplacement)
+    {
+        $AJOUTER_EMPLACEMENT = "INSERT INTO emplacement(longueur, largeur, estDisponible) VALUES (:longueur, :largeur; :estDisponible)";
+
+        global $basededonnees;
+
+        $requeteAjouterEmplacement = $basededonnees->prepare($AJOUTER_EMPLACEMENT);
+
+        $requeteAjouterEmplacement->bindValue(':longueur', $emplacement->getLongueur());
+        $requeteAjouterEmplacement->bindValue(':largeur', $emplacement->getLargeur());
+        $requeteAjouterEmplacement->bindValue(':estDisponible', $emplacement->getEstdisponible());
+
+
+        $requeteAjouterEmplacement->execute();
+    }
+
+    public function modifierEplacement(Emplacement $emplacement)
+    {
+        $MODIFIER_EMPLACEMENT = "UPDATE emplacement SET longueur = :longueur, largeur = :largeur WHERE idemplacement = :idemplacement";
+
+        global $basededonnees;
+
+        $requeteModifierEmplacement = $basededonnees->prepare($MODIFIER_EMPLACEMENT);
+
+        $requeteModifierEmplacement->bindValue(':longueur', $emplacement->getLongueur());
+        $requeteModifierEmplacement->bindValue(':largeur', $emplacement->getLargeur());
+        $requeteModifierEmplacement->bindValue(':estDisponible', $emplacement->getEstdisponible());
+        $requeteModifierEmplacement->bindValue(':idemplacement', $emplacement->getIdemplacement());
+
+        $requeteModifierEmplacement->execute();
+    }
+
+    public function trouverEmplacement($idemplacement)
+    {
+        global $basededonnees;
+        $TROUVER_EMPLACEMENT = 'SELECT * FROM emplacement WHERE idemplacement = :idemplacement';
+        $requeteTrouverEmplacement = $basededonnees->prepare($TROUVER_EMPLACEMENT);
+        $requeteTrouverEmplacement->bindValue(':idemplacement', $idemplacement);
+        $requeteTrouverEmplacement->execute();
+
+        return $requeteTrouverEmplacement->fetch(PDO::FETCH_OBJ);
+    }
+
+    public function supprimerEmplacement($idemplacement)
+    {
+        global $basededonnees;
+        $SUPPRIMER_EMPLACEMENT = 'DELETE FROM emplacement WHERE idemplacement = :idemplacement';
+        $requeteSupprimerEmplacement = $basededonnees->prepare($SUPPRIMER_EMPLACEMENT);
+        $requeteSupprimerEmplacement->bindValue(':idemplacement', $idemplacement);
+        $requeteSupprimerEmplacement->execute();
+    }
+
 }
