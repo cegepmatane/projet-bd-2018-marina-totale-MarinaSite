c417b663c59c243548911ec4dfb14ee2549e0fc3
Hugo BLANCHARD <zaturgo@gmail.com>
2018-10-16 17:03:00 -0400

    #10 bold footer

--- Site public/vue/pageTraversee.php
+++ Site public/vue/pageTraversee.php
@@ -77,7 +77,7 @@
 </div>
     <footer id="myFooter">
         <div class="w3-container w3-theme-l2 w3-padding-25" >
-            <h4 style="font-weight: bold;">La Marina de Matane, un espace de détente fort en couleurs!</h4>
+            <h4>La Marina de Matane, un espace de détente fort en couleurs!</h4>
         </div>
     </footer>
 
