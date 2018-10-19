3dc07a8f332deb81f8e3eb9c9650760f6e6b516f
Hugo BLANCHARD <zaturgo@gmail.com>
2018-10-19 19:38:02 -0400

    FIX GIT STASH LIBRAIRIES LOURDES

--- Site public/vue/vueAjouterEmplacement.php
+++ Site public/vue/vueAjouterEmplacement.php
@@ -51,7 +51,7 @@ if ((!empty($_POST['label'])) && (!empty($_POST['longueur'])) && (!empty($_POST[
 
 ?>
     <div class="ajouterEmplacement p-lg-5 p-md-3">
-<<<<<<< Updated upstream
+
         <fieldset>
             <legend>Ajouter un nouvel emplacement</legend>
 
@@ -141,30 +141,7 @@ if ((!empty($_POST['label'])) && (!empty($_POST['longueur'])) && (!empty($_POST[
         </div>
     </div>
 
-    <fieldset>
-        <legend>Ajouter un nouvel emplacement</legend>
 
-        <form action="vueAjouterEmplacement.php" method="post">
-            <label>Label:
-                <input class="form-control" type="text" name="label"
-                       value="<?php if (isset($_POST['label'])) echo $_POST['label'] ?>"/>
-            </label>
-            </br>
-            <label>Longueur:
-                <input class="form-control" type="text" name="longueur"
-                       value="<?php if (isset($_POST['longueur'])) echo $_POST['longueur'] ?>"/>
-            </label>
-            </br>
-            <label>Largeur:
-                <input class="form-control" type="text" name="largeur"
-                       value="<?php if (isset($_POST['largeur'])) echo $_POST['largeur'] ?>"/>
-            </label>
-
-            </br>
-
-            <input class="btn btn-primary" type="submit" name="ajouterEmplacement" value="Ajouter emplacement"/>
-
-        </form>
 
     </fieldset>
 
