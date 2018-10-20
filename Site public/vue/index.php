6de4686ca5d5bb2d4cfcc12c9680ed2a8fec24d1
Florent F <florent.fli@gmail.com>
2018-10-20 00:21:33 -0400

    #10 favicon page acceuil

--- Site public/vue/index.php
+++ Site public/vue/index.php
@@ -3,6 +3,7 @@
 <title>MarinaConnect</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="../img/marina.ico">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <body style="background-color: #e1f5fe;">
 
