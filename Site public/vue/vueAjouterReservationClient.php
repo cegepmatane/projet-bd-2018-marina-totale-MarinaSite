36e3780489024a74dfeed63bbde1cd5d051b3554
Hugo BLANCHARD <zaturgo@gmail.com>
2018-11-05 20:08:14 -0500

    #16 ajout des dates en post et page marche et passé no clickable

--- Site public/vue/vueAjouterReservationClient.php
+++ Site public/vue/vueAjouterReservationClient.php
@@ -52,7 +52,6 @@ if (isset($_POST['select_bateau'])) {
     }
 }
 
-
 //gestion erreurs
 
 
@@ -60,43 +59,16 @@ if ($dejaPost == 1) {
     if (!isset($_POST['select_bateau'])) {
         $erreurs['select_bateau'] = "<div class=\"alert alert-danger\">Veuillez selectionnez un bateau</div>";
     }
-    /*if (!isset($dateFin) || !preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $dateFin)) {
-        $erreurs['format_date_debut'] = '<div class="alert alert-danger">Veuillez rentrer une date d\'arrivé valide au format YYYY-MM-DD</div>';
-    }
-    if (!isset($dateDebut) || !preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $dateDebut)) {
-        $erreurs['format_date_fin'] = '<div class="alert alert-danger">Veuillez rentrer une date de départ valide au format YYYY-MM-DD</div>';
-    }*/
-
-    /* if (isset($dateFin) && isset($dateDebut) && preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $dateDebut)
-         && preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $dateFin)) {
-         if (!dateCompareAujourdhui($dateDebut)) {
-             $erreurs['dateCompareAujourdhui'] = "<div class=\"alert alert-danger\">La date ne peu pas etre avant la date d'aujourdhui</div>";
-         }
-         if (!dateCompare($dateDebut, $dateFin)) {
-             $erreurs['date_compare'] = "<div class=\"alert alert-danger\">La date d'arrivé doit être posterieur de la date de départ</div>";
-         }
-         if (!checkDateAAAAMMDD($dateDebut)) {
-             $erreurs['format_date_debut'] = '<div class="alert alert-danger">Veuillez rentrer une date d\'arrivé valide au format YYYY-MM-DD</div>';
-         }
-         if (!checkDateAAAAMMDD($dateFin)) {
-             $erreurs['format_date_fin'] = '<div class="alert alert-danger">Veuillez rentrer une date de départ valide au format YYYY-MM-DD</div>';
-         }
-
-         if (isset($id_bateau)) {
-             if (bateauEstDejaReserverSelonDate($dateDebut, $dateFin, $id_bateau)) {
-                 $erreurs['bateau_indisponible'] = "<div class=\"alert alert-danger\">Votre bateau est deja réserver sur un emplacement entre ces dates là</div>";
-             }
-         }
-     }*/
 }
 
 if ((isset($dateDebut)) && (isset($dateFin)) && (isset($id_bateau))
-    && checkDateAAAAMMDD($dateDebut) && checkDateAAAAMMDD($dateFin)
-    && dateCompare($dateDebut, $dateFin)
-    && dateCompareAujourdhui($dateDebut) && dateCompareAujourdhui($dateFin)
-    && !bateauEstDejaReserverSelonDate($dateDebut, $dateFin, $id_bateau)
+//    && checkDateAAAAMMDD($dateDebut) && checkDateAAAAMMDD($dateFin)
+//    && dateCompare($dateDebut, $dateFin)
+//    && dateCompareAujourdhui($dateDebut) && dateCompareAujourdhui($dateFin)
+//    && !bateauEstDejaReserverSelonDate($dateDebut, $dateFin, $id_bateau)
     && preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $dateDebut)
-    && preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $dateFin)) {
+    && preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $dateFin)
+    && isset($_POST["ajouterReservation"])) {
 
 
     if (empty($erreurs)) {
@@ -158,9 +130,9 @@ function emplacementValide($dateDebut, $dateFin, $idbateau)
         // LISTE DES EMPLACEMENT DISPO SELON DATE
         if ($emplacementDAO->checkTailleEmplacementSelonBateau($idbateau, $emplacement)) {
             return $emplacement->id;
-        } else {
+        } /*else {
             return -1;
-        }
+        }*/
     }
     return 0;
 }
@@ -176,13 +148,15 @@ if (isset($_POST["select_bateau"])) {
     $emplacementDAO = new EmplacementDAO();
     $bateauDAO = new BateauDAO();
     $tabJour = array();
+    $tabJourPasse = array();
     $bateau = $bateauDAO->trouverBateau($_POST["select_bateau"]);
     $nbEmplacementBonneTaille = $emplacementDAO->compterEmplacementSelonTaille($bateau->longueur, $bateau->largeur);
     $donneesReservations = $reservationDAO->listerReservationsSelonTaille($bateau->longueur, $bateau->largeur);
     $count = 0;
     $ajd = date('Y-m-d');
-    for ($jour = 0; $jour < 365; $jour++) {
+    for ($jour = 0; $jour < 10000; $jour++) {
         $dateJour = date('Y-m-d', strtotime($ajd . "+" . $jour . "days"));
+        $dateJourPasse = date('Y-m-d', strtotime($ajd . "-" . $jour . "days"));
         foreach ($donneesReservations as $reservation) {
             if ($reservation->datedebut <= $dateJour && $reservation->datefin >= $dateJour) {
                 $count++;
@@ -191,8 +165,10 @@ if (isset($_POST["select_bateau"])) {
         if ($count == $nbEmplacementBonneTaille->nombre) {
             $tabJour[] = $dateJour;
         }
+        $tabJourPasse[] = $dateJourPasse;
         $count = 0;
     }
+
 
     ?>
     <script>
@@ -213,6 +189,12 @@ if (isset($_POST["select_bateau"])) {
                             rendering: 'background'
                           },";
                 }?>
+                    <?php foreach ($tabJourPasse as $event) {
+                    echo "{ 
+                            start:'" . $event . "',
+                            rendering: 'background'
+                          },";
+                }?>
 
                 ],
                 selectable: true,
@@ -220,6 +202,9 @@ if (isset($_POST["select_bateau"])) {
                     // alert('clicked ' + date.format());
                 },
                 select: function (startDate, endDate) {
+                    console.log("datedebut: " + startDate.format() + "  datefin: " + endDate.format());
+                    document.getElementById('dateDebut').value = startDate.format();
+                    document.getElementById('dateFin').value = endDate.format();
                     // alert('selected ' + startDate.format() + ' to ' + endDate.format());
                 },
                 selectOverlap: function (event) {
@@ -241,47 +226,24 @@ if (isset($_POST["select_bateau"])) {
 
             <form action="vueAjouterReservationClient.php?id=<?php echo $_SESSION['id'] ?>" method="post">
 
-                <!--<div class="form-group">
-                    <label>Date d'arrivé:</label>
-                    <input type="date" name="dateDebut"
-                           value="<?php /*if (isset($_POST['dateDebut'])) echo $_POST['dateDebut'] */ ?>"/>
-                </div>
 
-                <?php /*if (isset($erreurs['format_date_debut'])) {
-                    echo $erreurs['format_date_debut'];
-                } */ ?>
+                    <div class="row justify-content-start">
+                        <label>Bateau : </label>
+                        <select name="select_bateau" required>
+                            <?php if (isset($donneesBateaux[0])): ?>
+                                <option value="0" disabled selected>- SELECTIONNEZ BATEAU -</option>
+                                <?php foreach ($donneesBateaux as $bateau) : ?>
+                                    <option <?php echo(isset($id_bateau) && ($id_bateau == $bateau->id) ? ' selected="selected"' : ''); ?>
+                                            value="<?php echo $bateau->id ?>"><?php echo $bateau->nom . ' (' . $bateau->type_bateau . ')' ?></option>
+                                <?php endforeach; ?>
+                            <?php else: ?>
+                                <option value="">Pas de bateaux...</option>
+                            <?php endif; ?>
+                        </select>
 
-                <div class="form-group">
-                    <label>Date de départ:</label>
-                    <input type="date" name="dateFin"
-                           value="<?php /*if (isset($_POST['dateFin'])) echo $_POST['dateFin'] */ ?>"/>
-                </div>
+                    </div>
 
-                <?php /*if (isset($erreurs['format_date_fin'])) {
-                    echo $erreurs['format_date_fin'];
-                } */ ?>
-                <?php /*if (isset($erreurs['dateCompareAujourdhui'])) {
-                    echo $erreurs['dateCompareAujourdhui'];
-                } */ ?>
-                --><?php /*if (isset($erreurs['date_compare'])) {
-                    echo $erreurs['date_compare'];
-                } */ ?>
 
-                <div class="form-group">
-                    <!--                    <label>Bateau : </label>
-                    <select name="select_bateau" required>
-                        <?php /*if (isset($donneesBateaux[0])): */ ?>
-                            <option value="0" disabled selected>- SELECTIONNEZ BATEAU -</option>
-                            <?php /*foreach ($donneesBateaux as $bateau) : */ ?>
-                                <option <?php /*echo(isset($id_bateau) && ($id_bateau == $bateau->id) ? ' selected="selected"' : ''); */ ?>
-                                        value="<?php /*echo $bateau->id */ ?>"><?php /*echo $bateau->nom . ' (' . $bateau->type_bateau . ')' */ ?></option>
-                            <?php /*endforeach; */ ?>
-                        <?php /*else: */ ?>
-                            <option value="">Pas de bateaux...</option>
-                        <?php /*endif; */ ?>
-                    </select>
-                </div>
--->
                     <?php if (isset($erreurs['bateau_indisponible'])) {
                         echo $erreurs['bateau_indisponible'];
                     } ?>
@@ -308,7 +270,12 @@ if (isset($_POST["select_bateau"])) {
                         <label>Essence :</label>
                         <input type="checkbox" name="essence" <?php if ($essence == 1) echo ' checked' ?>/>
                     </div>
-
+                    <input type="hidden" id="dateDebut" name="dateDebut">
+                    <input type="hidden" id="dateFin" name="dateFin">
+                <div class="col-sm-4">
+                    <input class="btn btn-outline-primary btn-sm m-1" type="submit" name="submit"
+                           value="Actualiser le calendrier">
+                </div>
                     <input type="submit" class="btn btn-primary" name="ajouterReservation"
                            value="Effectuer une demande de réservation"/>
 
@@ -320,28 +287,6 @@ if (isset($_POST["select_bateau"])) {
 
         </fieldset>
 
-
-        <div class="container">
-            <form action="vueAjouterReservationClient.php" method="post">
-                <div class="row justify-content-start">
-                    <select name="select_bateau" required>
-                        <?php if (isset($donneesBateaux[0])): ?>
-                            <option value="0" disabled selected>- SELECTIONNEZ BATEAU -</option>
-                            <?php foreach ($donneesBateaux as $bateau) : ?>
-                                <option <?php echo(isset($id_bateau) && ($id_bateau == $bateau->id) ? ' selected="selected"' : ''); ?>
-                                        value="<?php echo $bateau->id ?>"><?php echo $bateau->nom . ' (' . $bateau->type_bateau . ')' ?></option>
-                            <?php endforeach; ?>
-                        <?php else: ?>
-                            <option value="">Pas de bateaux...</option>
-                        <?php endif; ?>
-                    </select>
-                    <div class="col-sm-4">
-                        <input class="btn btn-outline-primary btn-sm m-1" type="submit" name="submit"
-                               value="Actualiser le calendrier">
-                    </div>
-                </div>
-            </form>
-        </div>
         <?php if (isset($_POST["select_bateau"])) {
             ?>
             <div id='calendar' class="p-5"></div><?php
